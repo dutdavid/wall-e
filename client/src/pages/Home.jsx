9128eebd7479b194b9e9a49ae453f10959cb10e3
Dut <dutdavid7@gmail.com>
2023-02-25 18:45:50 +0000

    create Loading and allposts usestate in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useffect } from 'react'
+import { Loader, Card, FormField } from '../components'
 
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null)
   return (
     <div>Home</div>
   )

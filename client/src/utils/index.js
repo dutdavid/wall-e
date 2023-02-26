631a89910cd1b60b36eb1360f55e166f45dfa388
Dut <dutdavid7@gmail.com>
2023-02-26 19:11:19 +0000

    utils

---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompts(prompt) {
+    const randomIndex = Math.floor(Math.random() *
+    surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    return randomPrompt;
+}

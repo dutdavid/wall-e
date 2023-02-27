b6e9809a5e928c6df9d571f5f729cb354f2648c5
Dut <dutdavid7@gmail.com>
2023-02-27 20:28:15 +0000

    fixed handleChange error

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -5,9 +5,8 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
     <div>
       <div className="flex items-center gap-2 mb-2">
         <label 
-        htmlFor="{name}"
-        className="block text-sm font-medium text-gray-900"
-        >
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-900">
           {labelName}
         </label>
         {isSurpriseMe && (
@@ -21,12 +20,13 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
       </div>
       <input
       type={type}
-      id={id}
+      id={name}
       name={name}
       placeholder={placeholder}
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
       required
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
       />
     </div>
   )

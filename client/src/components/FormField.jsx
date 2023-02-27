97f323d552edf9edd0e8509dade2bbd07fe802f5
Dut <dutdavid7@gmail.com>
2023-02-27 20:06:17 +0000

    surprise me button

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -10,7 +10,14 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         >
           {labelName}
         </label>
-      
+        {isSurpriseMe && (
+          <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black">
+            Surprise Me
+          </button>
+        )}
       </div>
     </div>
   )

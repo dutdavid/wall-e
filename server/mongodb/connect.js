54ba8c480456fba6b4308d68fcbe9f5150b71e86
dutdavid <dutdavid7@gmail.com>
2023-03-02 00:11:35 +0300

    mongo

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -2,4 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = (url) => {
     mongoose.set('strictQuery', true);
+    mongoose.connect(url)
+    .then(() => console.log('Mongo Connected'))
+    .catch((err) => console.log(err));
+
 }

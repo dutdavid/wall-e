93ec16b6e66721c5ae20f6db41ce0da0816d8c5f
Dut <dutdavid7@gmail.com>
2023-03-10 19:35:39 +0000

    try catch

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -26,8 +26,9 @@ router.route('/').post( async (req, res) => {
 
     const image = aiResponse.data.data[0].b64_json;
 
+    res.status(200).json({ photo: image });
    } catch (error) {
-
+    res.status(500).send(error?.response.data.error.message)
    }
 })
 

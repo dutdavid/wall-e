0f0d88ddc4cc54a71aeefcb89ed54009d03bd376
dutdavid <dutdavid7@gmail.com>
2023-03-20 22:50:56 +0300

    async

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,6 +28,10 @@ const CreatePost = () => {
           },
           body: JSON.stringify({ prompt: form.prompt }),
         })
+
+        const data = await response.json();
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,&{data.photo}`})
       } catch (error) {
 
       }

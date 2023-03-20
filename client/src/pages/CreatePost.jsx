41da9afe78b2062f378f027a234fc6d967a60305
dutdavid <dutdavid7@gmail.com>
2023-03-20 22:43:43 +0300

    try catch

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,6 +21,9 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+        })
       } catch (error) {
 
       }

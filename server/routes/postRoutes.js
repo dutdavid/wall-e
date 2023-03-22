a6629ed05b3272e8b70c4b08c525c068a333f727
Dut <dutdavid7@gmail.com>
2023-03-22 10:30:27 +0000

    try catch on post routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -19,16 +19,20 @@ const router = express.Router();
 
  // CREATE A POST
 router.route('/').post(async(req, res) => {
-   const { name, prompt, photo } = req.body;
-   const photoUrl = await cloudinary.uploader.upload(photo);
-
-   const newPost = await Post.create({
-      name,
-      prompt,
-      photo: photoUrl.url,
-   })
-
-   res.status(201).json({ success: true, data: newPost });
-});
-
+   try {
+      const { name, prompt, photo } = req.body;
+      const photoUrl = await cloudinary.uploader.upload(photo);
+   
+      const newPost = await Post.create({
+         name,
+         prompt,
+         photo: photoUrl.url,
+      })
+   
+      res.status(201).json({ success: true, data: newPost });
+   } catch (error) {
+      res.status(500).json({ success: false, message: error})
+   }
+   });
+ 
 export default router;
